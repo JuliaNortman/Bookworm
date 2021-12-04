e1bae2e2f1ea2baf9f0ce43d34a9c53ee3a088ed
JuliaNortman <ynortman@gmail.com>
2021-12-04 14:44:11 +0200

    Fix public key

--- marketplace-client/src/marketplace/src/environments/environment.prod.ts
+++ marketplace-client/src/marketplace/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://bookworm-marketplace.herokuapp.com/api',
-  captchaKey: '6LeCJUcbAAAAAJbo6GJSJb1EIU-ugiU2AW-MdfBX'
+  captchaKey: '6LeJeXkdAAAAALbliP7gx60otChyDP5W9Jo9zRn8'
 };
